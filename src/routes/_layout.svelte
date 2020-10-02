797dcbf77c556b563b713035401dda9ae9703aaa
george-aprozeanu <george.aprozeanu@gmail.com>
2020-10-02 10:51:34 +0300

    Increment

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,15 +1,23 @@
 <script>
-	import Nav from '../components/nav.svelte';
-
-	export let segment;
+  export let segment
 </script>
 
 <style>
-	
+  :global(body) {
+    margin: 0;
+    font-family: system-ui, BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, sans-serif;
+    min-height: 100vh;
+    background-color: white;
+    color: #303030;
+  }
+  @media (prefers-color-scheme: dark) {
+    :global(body) {
+      background-color: #222;
+      color: lightslategray;
+    }
+  }
 </style>
 
-<Nav {segment}/>
-
 <main>
-	<slot></slot>
+  <slot />
 </main>
--- src/routes/games/tetris.svelte
+++ src/routes/games/tetris.svelte
@@ -1,37 +1,70 @@
-<style>
-    .scene {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .game {
-        border: solid 1px slategray;
-        outline: solid 6px lightblue;
-    }
-</style>
 <script>
-    const scene = {
-        sceneWidth: 10,
-        sceneHeight: 20,
-        squareWidth: 35,
-        squareHeight: 35
+  const scene = {
+    sceneWidth: 10,
+    sceneHeight: 20,
+    squareWidth: 35,
+    squareHeight: 35
+  }
+  let shapes = [
+    {
+      pos: { x: 1, y: 1 },
+      rects: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 1, y: 1 }
+      ]
+    },
+    {
+      pos: { x: 1, y: 5 },
+      rects: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 1, y: 1 }
+      ]
     }
-    let rects = [
-        { x: 1, y: 1 },
-        { x: 2, y: 1 },
-        { x: 3, y: 1 },
-        { x: 2, y: 2 },
-
-        { x: 1, y: 5 },
-        { x: 2, y: 5 },
-        { x: 3, y: 5 },
-        { x: 2, y: 6 },
-    ]    
+  ]
 </script>
+
+<style>
+  .scene {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+  }
+  .game {
+    border: solid 1px slategray;
+    outline: solid 6px lightblue;
+  }
+  .square {
+    stroke: slategray;
+    fill: lightblue;
+  }
+  @media (prefers-color-scheme: dark) {
+    .game {
+      border: solid 1px black;
+      outline: solid 6px darkslategray;
+    }
+    .square {
+      stroke: #111;
+      fill: darkslategray;
+    }
+  }
+</style>
+
 <section class="scene">
-<svg width={scene.sceneWidth * scene.squareWidth} height={scene.squareHeight * scene.sceneHeight} class="game">
-    {#each rects as rect}
-        <rect x={rect.x * scene.squareWidth} y={rect.y * scene.squareHeight} width={scene.squareWidth} height={scene.squareHeight} stroke="#444" fill="none"  ></rect>
+  <svg width={scene.sceneWidth * scene.squareWidth} height={scene.squareHeight * scene.sceneHeight} class="game">
+    {#each shapes as shape}
+      {#each shape.rects as rect}
+        <rect
+          class="square"
+          x={(shape.pos.x + rect.x) * scene.squareWidth}
+          y={(shape.pos.y + rect.y) * scene.squareHeight}
+          width={scene.squareWidth}
+          height={scene.squareHeight} />
+      {/each}
     {/each}
-</svg>
+  </svg>
 </section>
